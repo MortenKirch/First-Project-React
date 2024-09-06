247569d7a69f1f693cac70da5720eaca93ee220e
MortenKirch <morten-kris@live.dk>
2024-09-06 13:02:40 +0200

    init

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { defineConfig } from "vite";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/first-project/"}>
+    <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import react from "@vitejs/plugin-react"; // this on might be "import react from "@vitejs/plugin-react";" in your solution
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+    const config = {
+        plugins: [react()],
+        base: "/"
+    };
+
+    if (command !== "serve") {
+        config.base = "/first-project/";
+    }
+
+    return config;
+});
